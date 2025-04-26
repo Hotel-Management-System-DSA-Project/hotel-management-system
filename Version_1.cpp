506b8ddc925ec1715cc6182f0c25c7e047d42727
Pham Trieu Minh <trieuminhpham2808@gmail.com>
2025-04-26 07:42:16 +0700

    Them Version_1

---
+++ Version_1.cpp
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <iostream>
+using namespace std;
+
+enum roomStatus {ready, wait, inUse};
+enum Type {vip, single, double};
+
+struct Room {
+    int ID;
+    int Price;
+    roomStatus stt;
+    Type type;
+};
+
+struct Custom {
+    char name[20];
+    int age;
+    int phoneNumber;
+    int identify;
+    int timeUse;
+};
+
+int main() {
+    cout << "Test code";
+    return 0;
+}
